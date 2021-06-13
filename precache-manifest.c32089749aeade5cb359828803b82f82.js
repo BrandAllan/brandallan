a03861c2cdb30e523713e871457cb3d51ba31478
Danny <dannytownend2004@gmail.com>
2021-06-14 00:14:04 +0100

    Updates

---
+++ precache-manifest.c32089749aeade5cb359828803b82f82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2087f27e82c49ecd6078",
+    "url": "/brandallan/static/css/main.117ec2e5.chunk.css"
+  },
+  {
+    "revision": "2087f27e82c49ecd6078",
+    "url": "/brandallan/static/js/main.2087f27e.chunk.js"
+  },
+  {
+    "revision": "05f10e3509814ff01886",
+    "url": "/brandallan/static/js/1.05f10e35.chunk.js"
+  },
+  {
+    "revision": "ac5be962dc6af17dd43b",
+    "url": "/brandallan/static/js/runtime~main.ac5be962.js"
+  },
+  {
+    "revision": "952542d7e17ae99abfbe9f4ff6aa903f",
+    "url": "/brandallan/static/media/corn.952542d7.jpg"
+  },
+  {
+    "revision": "4e39f849e36cd5fcda6fe6c03fdf8816",
+    "url": "/brandallan/static/media/ditto.4e39f849.jpg"
+  },
+  {
+    "revision": "557f47ad0d6062ec1837b863bdc84328",
+    "url": "/brandallan/index.html"
+  }
+];
