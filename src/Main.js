0347d193e6a4e7f1a441bf20400fb6a5838d54aa
Danny <dannytownend2004@gmail.com>
2021-06-13 23:07:18 +0100

    Removing Hashrouter and using BrowserRouter and Switch

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,6 @@
 import React, {Component} from "react";
 
-import {
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from "./Home";
 import Trains from "./Trains";
 import Tanks from "./Tanks";
@@ -18,31 +14,36 @@ import Shape from "./Shape";
 class Main extends Component {
   render() {
     return (
-      <HashRouter>
+      <Router>
         <div>
-          <h1>Stuff that is #BrandAllan</h1>
-          <ul className="header">
-            <li><NavLink exact to="/">Home</NavLink></li>
-            <li><NavLink to="/trains">Trains</NavLink></li>
-            <li><NavLink to="/tanks">Tanks</NavLink></li>
-            <li><NavLink to="/corn">Corn</NavLink></li>
-            <li><NavLink to="/strudel">Strudel</NavLink></li>
-            <li><NavLink to="/ditto">Ditto</NavLink></li>
-            <li><NavLink to="/brbrbrbr">Brbrbrbr</NavLink></li>
-            <li><NavLink to="/shape">Shape</NavLink></li>
-          </ul>
-          <div className="content">
-            <Route exact path="/" component={Home}/>
-            <Route path="/trains" component={Trains}/>
-            <Route path="/tanks" component={Tanks}/>
-            <Route path="/corn" component={Corn}/>
-            <Route path="/strudel" component={Strudel}/>
-            <Route path="/ditto" component={LumpyDitto}/>
-            <Route path="/brbrbrbr" component={Brbrbrbr}/>
-            <Route path="/shape" component={Shape}/>
-          </div>
+          <nav>
+            <h1>Stuff that is #BrandAllan</h1>
+            <ul className="header">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/trains">Trains</Link></li>
+              <li><Link to="/tanks">Tanks</Link></li>
+              <li><Link to="/corn">Corn</Link></li>
+              <li><Link to="/strudel">Strudel</Link></li>
+              <li><Link to="/ditto">Ditto</Link></li>
+              <li><Link to="/brbrbrbr">Brbrbrbr</Link></li>
+              <li><Link to="/shape">Shape</Link></li>
+            </ul>
+          </nav>
+
+            {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+          <Switch>
+            <Route exact path="/"><Home/></Route>
+            <Route exact path="/trains"><Trains/></Route>
+            <Route exact path="/tanks"><Tanks/></Route>
+            <Route exact path="/corn"><Corn/></Route>
+            <Route exact path="/strudel"><Strudel/></Route>
+            <Route exact path="/ditto"><LumpyDitto/></Route>
+            <Route exact path="/brbrbrbr"><Brbrbrbr/></Route>
+            <Route exact path="/shape"><Shape/></Route>
+          </Switch>
         </div>
-      </HashRouter>
+      </Router>
     );
   }
 }
